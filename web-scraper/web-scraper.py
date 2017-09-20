88c783d81e7fe4ce4627e84aad46e7b7935d7de9
Zack Littke-Smith <zack.littkesmith@gmail.com>
2017-09-19 23:37:27 -0400

    Finishing scraper

--- web-scraper/web-scraper.py
+++ web-scraper/web-scraper.py
@@ -9,20 +9,24 @@ this bearer auth token will have to change for consecutive runs. to update it, v
 
 https://gatech-primo.hosted.exlibrisgroup.com/primo-explore/search?query=sub,exact,Science%20Fiction,AND&pfilter=lang,exact,eng,AND&pfilter=pfilter,exact,book,AND&tab=default_tab&search_scope=default_scope&sortby=rank&vid=01GALI_GIT&mfacet=tlevel,include,available,1&mfacet=domain,include,Science%20Fiction%20Collection,1&mfacet=rtype,include,book,1&mfacet=library,include,GTMAIN,1&lang=en_US&mode=advanced&offset=0
 """
-token = "Bearer eyJraWQiOiJwcmltb0V4cGxvcmVQcml2YXRlS2V5LTAxR0FMSV9HSVQiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJQcmltbyIsImp0aSI6IiIsImV4cCI6MTUwNTg1NDQ0MiwiaWF0IjoxNTA1NzY4MDQyLCJ1c2VyIjoiYW5vbnltb3VzLTA5MThfMjA1NDAyIiwidXNlck5hbWUiOm51bGwsInVzZXJHcm91cCI6IkdVRVNUIiwiYm9yR3JvdXBJZCI6bnVsbCwidWJpZCI6bnVsbCwiaW5zdGl0dXRpb24iOiIwMUdBTElfR0lUIiwidmlld0luc3RpdHV0aW9uQ29kZSI6IjAxR0FMSV9HSVQiLCJpcCI6IjE0My4yMTUuMjcuMzgiLCJvbkNhbXB1cyI6ImZhbHNlIiwibGFuZ3VhZ2UiOiJlbl9VUyIsImF1dGhlbnRpY2F0aW9uUHJvZmlsZSI6IiIsInZpZXdJZCI6IjAxR0FMSV9HSVQiLCJpbHNBcGlJZCI6bnVsbH0.cBAWcWvqfZh6BfwQRYKWYOPqzU9T-HPI0C8VVwJEX5Po_8lgErRE53_42Xb31ZzbDeltyNyt7rmG3Djglbz6hA"
+token = "Bearer eyJraWQiOiJwcmltb0V4cGxvcmVQcml2YXRlS2V5LTAxR0FMSV9HSVQiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJQcmltbyIsImp0aSI6IiIsImV4cCI6MTUwNTk1OTMwMSwiaWF0IjoxNTA1ODcyOTAxLCJ1c2VyIjoiYW5vbnltb3VzLTA5MjBfMDIwMTQxIiwidXNlck5hbWUiOm51bGwsInVzZXJHcm91cCI6IkdVRVNUIiwiYm9yR3JvdXBJZCI6bnVsbCwidWJpZCI6bnVsbCwiaW5zdGl0dXRpb24iOiIwMUdBTElfR0lUIiwidmlld0luc3RpdHV0aW9uQ29kZSI6IjAxR0FMSV9HSVQiLCJpcCI6Ijc2Ljk3LjY3LjIwNSIsIm9uQ2FtcHVzIjoiZmFsc2UiLCJsYW5ndWFnZSI6ImVuX1VTIiwiYXV0aGVudGljYXRpb25Qcm9maWxlIjoiIiwidmlld0lkIjoiMDFHQUxJX0dJVCIsImlsc0FwaUlkIjpudWxsfQ.znDIsJsoFDsIZ0WGeveSY9uFGh074ycVP6xuxy4uE1HjiaGVz5HV-2TYGh7JaRBZw4vAJqhwODqxI5Nf-qBWRA"
 
-r = json.load(requests.get(requestURL, headers={'Authorization': 'TOK:<%s>' % token}).text)
+r = json.loads(requests.get(requestURL, headers={'Authorization': token}).text)
 
 books = []
 
 for entry in r["docs"]:
 	info = entry["pnx"]["display"]
 
-	if "Science Fiction, 4th Floor West" in info["availlibrary"]:
+	if any("Science Fiction, 4th Floor West" in loc for loc in info["availlibrary"]):
+		author = ', '.join(x.encode('utf-8') for x in info["creator"]) if "creator" in info else None
+		title = ', '.join(x.encode('utf-8') for x in info["title"]) if "title" in info else None
+		identifier = ', '.join(x.encode('utf-8') for x in info["identifier"]).replace('$$CISBN$$', 'ISBN ') if "identifier" in info else None
 		books.append({
-			"author" : info["creator"],
-			"title" : info["title"],
-			"isbn" : info["identifier"]
+			"author" : author,
+			"title" : title,
+			"isbn" : identifier
 		})
 
-print books
+with open('books.json', 'wb') as file:
+    json.dump(books, file)
