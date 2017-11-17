afc5a1657dc3a16d5599263a7ea725ba10110405
Daniel Marcos <danielms215@gmail.com>
2017-11-16 20:16:53 -0500

    Set up tap to alternate between row and aisle view

--- Glass/glass/src/main/java/indoornavigation/com/glass/MainActivityGlass.java
+++ Glass/glass/src/main/java/indoornavigation/com/glass/MainActivityGlass.java
@@ -2,11 +2,13 @@ package indoornavigation.com.glass;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MainActivityGlass extends Activity {
@@ -34,84 +36,90 @@ public class MainActivityGlass extends Activity {
     TextView isbnText;
     TextView titleText;
     TextView authorText;
-
     TextView instructionText;
+
+    enum PathView {
+        AISLE,
+        ROW
+    }
+    PathView currentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        currentView = PathView.AISLE;
 
         setContentView(R.layout.activity_main);
-       // parentView = findViewById(R.id.parent_view);
-//
-//        aisleView = findViewById(R.id.aisle_view);
-//        letterViews = new View[] {
-//                findViewById(R.id.block_a),
-//                findViewById(R.id.block_b),
-//                findViewById(R.id.block_c),
-//                findViewById(R.id.block_d),
-//                findViewById(R.id.block_e),
-//                findViewById(R.id.block_f)
-//        };
-//
-//        rowView = findViewById(R.id.row_view);
-//        blockViews = new View[][] {
-//                new View[] {
-//                        findViewById(R.id.block_0_0),
-//                        findViewById(R.id.block_0_1),
-//                        findViewById(R.id.block_0_2),
-//                        findViewById(R.id.block_0_3),
-//                        findViewById(R.id.block_0_4),
-//                        findViewById(R.id.block_0_5)
-//                },
-//                new View[] {
-//                        findViewById(R.id.block_1_0),
-//                        findViewById(R.id.block_1_1),
-//                        findViewById(R.id.block_1_2),
-//                        findViewById(R.id.block_1_3),
-//                        findViewById(R.id.block_1_4),
-//                        findViewById(R.id.block_1_5)
-//                },
-//                new View[] {
-//                        findViewById(R.id.block_2_0),
-//                        findViewById(R.id.block_2_1),
-//                        findViewById(R.id.block_2_2),
-//                        findViewById(R.id.block_2_3),
-//                        findViewById(R.id.block_2_4),
-//                        findViewById(R.id.block_2_5)
-//                },
-//                new View[] {
-//                        findViewById(R.id.block_3_0),
-//                        findViewById(R.id.block_3_1),
-//                        findViewById(R.id.block_3_2),
-//                        findViewById(R.id.block_3_3),
-//                        findViewById(R.id.block_3_4),
-//                        findViewById(R.id.block_3_5)
-//                },
-//                new View[] {
-//                        findViewById(R.id.block_4_0),
-//                        findViewById(R.id.block_4_1),
-//                        findViewById(R.id.block_4_2),
-//                        findViewById(R.id.block_4_3),
-//                        findViewById(R.id.block_4_4),
-//                        findViewById(R.id.block_4_5)
-//                },
-//                new View[] {
-//                        findViewById(R.id.block_5_0),
-//                        findViewById(R.id.block_5_1),
-//                        findViewById(R.id.block_5_2),
-//                        findViewById(R.id.block_5_3),
-//                        findViewById(R.id.block_5_4),
-//                        findViewById(R.id.block_5_5)
-//                },
-//        };
-//
-//        locationText = findViewById(R.id.location_text);
-//        isbnText = findViewById(R.id.isbn_text);
-//        titleText = findViewById(R.id.title_text);
-//        authorText = findViewById(R.id.author_text);
-//
-//        instructionText = findViewById(R.id.instruction_text);
+        parentView = findViewById(R.id.parent_view);
+
+        aisleView = findViewById(R.id.aisle_view);
+        letterViews = new View[] {
+                findViewById(R.id.block_a),
+                findViewById(R.id.block_b),
+                findViewById(R.id.block_c),
+                findViewById(R.id.block_d),
+                findViewById(R.id.block_e),
+                findViewById(R.id.block_f)
+        };
+
+        rowView = findViewById(R.id.row_view);
+        blockViews = new View[][] {
+                new View[] {
+                        findViewById(R.id.block_0_0),
+                        findViewById(R.id.block_0_1),
+                        findViewById(R.id.block_0_2),
+                        findViewById(R.id.block_0_3),
+                        findViewById(R.id.block_0_4),
+                        findViewById(R.id.block_0_5)
+                },
+                new View[] {
+                        findViewById(R.id.block_1_0),
+                        findViewById(R.id.block_1_1),
+                        findViewById(R.id.block_1_2),
+                        findViewById(R.id.block_1_3),
+                        findViewById(R.id.block_1_4),
+                        findViewById(R.id.block_1_5)
+                },
+                new View[] {
+                        findViewById(R.id.block_2_0),
+                        findViewById(R.id.block_2_1),
+                        findViewById(R.id.block_2_2),
+                        findViewById(R.id.block_2_3),
+                        findViewById(R.id.block_2_4),
+                        findViewById(R.id.block_2_5)
+                },
+                new View[] {
+                        findViewById(R.id.block_3_0),
+                        findViewById(R.id.block_3_1),
+                        findViewById(R.id.block_3_2),
+                        findViewById(R.id.block_3_3),
+                        findViewById(R.id.block_3_4),
+                        findViewById(R.id.block_3_5)
+                },
+                new View[] {
+                        findViewById(R.id.block_4_0),
+                        findViewById(R.id.block_4_1),
+                        findViewById(R.id.block_4_2),
+                        findViewById(R.id.block_4_3),
+                        findViewById(R.id.block_4_4),
+                        findViewById(R.id.block_4_5)
+                },
+                new View[] {
+                        findViewById(R.id.block_5_0),
+                        findViewById(R.id.block_5_1),
+                        findViewById(R.id.block_5_2),
+                        findViewById(R.id.block_5_3),
+                        findViewById(R.id.block_5_4),
+                        findViewById(R.id.block_5_5)
+                },
+        };
+
+        locationText = findViewById(R.id.location_text);
+        isbnText = findViewById(R.id.isbn_text);
+        titleText = findViewById(R.id.title_text);
+        authorText = findViewById(R.id.author_text);
+
+        instructionText = findViewById(R.id.instruction_text);
     }
 
     // todo this needs to be called by user input
@@ -119,6 +127,12 @@ public class MainActivityGlass extends Activity {
         // todo debug this
         currentUser = database.getReference("ar-order-picking").push().getKey();
         generateNextPick();
+    }
+
+    private void displayAisleView() {
+        currentView = PathView.AISLE;
+        rowView.setVisibility(View.GONE);
+        aisleView.setVisibility(View.VISIBLE);
     }
 
     // todo this needs to be called when a user has picked
@@ -140,4 +154,28 @@ public class MainActivityGlass extends Activity {
         // todo: debug this
         database.getReference("ar-order-picking").child(currentUser).setValue(pickTimes);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+            if (currentView == PathView.AISLE) {
+                displayRowView();
+            } else {
+                // currentView = PathView.ROW
+                generateNextPick();
+                displayAisleView();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void displayRowView() {
+        currentView = PathView.ROW;
+        aisleView.setVisibility(View.GONE);
+        rowView.setVisibility(View.VISIBLE);
+        instructionText.setText("Tap to move to next book.");
+    }
+
+
 }
